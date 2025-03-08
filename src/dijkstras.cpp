9c175951d18dcf44c16baf5bb7c5025e1a36c58a
kai798798 <tingkc1@uci.edu>
2025-03-08 07:13:01 +0000

    Changes to extract_shortest_path

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -29,7 +29,11 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
 vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination){
     vector<int> path;
     //
-    if (previous[destination] == -1 && destination != 0)
+    if (destination == 0){
+        path.push_back(0);
+        return path;
+    }
+    if (previous[destination] == -1)
         return {};
     //
     for (int v = destination; v != -1; v = previous[v])
